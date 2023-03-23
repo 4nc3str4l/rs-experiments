cf4adcac28d326553e07cce1a747fbae1e7e88e1
4nc3str4l <cmuri_10@hotmail.com>
2023-03-23 17:33:15 +0100

    Starting to render the algorithm

--- maze-generator/src/main.rs
+++ maze-generator/src/main.rs
@@ -1,3 +1,70 @@
+//! Shows how to render simple primitive shapes with a single color.
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, window::PresentMode};
+
+const WINDOW_WIDTH: f32 = 640.;
+const WINDOW_HEIGHT: f32 = 640.;
+
+const MATRIX_WIDTH: u16 = 10;
+const MATRIX_HEIGHT: u16 = 10;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Maze Generator".into(),
+                resolution: (640., 640.).into(),
+                present_mode: PresentMode::AutoNoVsync,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    let tile_width = WINDOW_WIDTH / MATRIX_WIDTH as f32;
+    let tile_height = WINDOW_HEIGHT / MATRIX_HEIGHT as f32;
+
+    let matrix = construct_matrix();
+
+    for y in 0..MATRIX_HEIGHT {
+        for x in 0..MATRIX_WIDTH {
+            if matrix[xy_to_index(x as usize, y as usize)] {
+                commands.spawn(MaterialMesh2dBundle {
+                    mesh: meshes
+                        .add(shape::Quad::new(Vec2::new(tile_width - 1.0, tile_height - 1.0)).into())
+                        .into(),
+                    material: materials.add(ColorMaterial::from(Color::BLACK)),
+                    transform: Transform::from_translation(
+                            Vec3::new(-WINDOW_WIDTH / 2. + tile_width / 2.0 + tile_width * x as f32,
+                            -WINDOW_HEIGHT / 2. + tile_height / 2.0 + tile_height * y as f32, 
+                            0.)),
+                    ..default()
+                });
+            }
+
+        }
+    }
+}
+
+fn construct_matrix() -> Vec<bool> {
+    
+    let mut data = Vec::with_capacity((MATRIX_HEIGHT as usize) * (MATRIX_WIDTH as usize));
+    for _ in 0..MATRIX_HEIGHT {
+        for _ in 0..MATRIX_WIDTH {
+            data.push(rand::random());
+        }
+    }
+    data
+}
+
+fn xy_to_index(x: usize, y: usize) -> usize {
+    x + y * MATRIX_WIDTH as usize
 }
